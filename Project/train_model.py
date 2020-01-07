e4a0920c472548abc45a8103e9b07bdf211b875b
Nicolas Bartholy <nbartholy@gmail.com>
2020-01-07 23:05:54 +0100

    Added train script for the model
    Added train script. Currently only loads latest model and then saves it again

---
+++ Project/train_model.py
@@ -0,0 +1,39 @@
+import os
+import argparse as ap
+import fileUtil
+from datetime import datetime
+import tensorflow as tf
+
+model_path = ''
+
+# Trains the specified model
+# if no arguments are given: trains the latest model
+if __name__ == '__main__':
+    cparse = ap.ArgumentParser(prog='train_model', description='Trains an existing model')
+    cparse.add_argument('--model', help='Path to the model', default='')
+
+    args = cparse.parse_args()
+    if args.model == '':
+        models = os.listdir(fileUtil.model_directory)
+        if not models:
+            print("No models found at " + fileUtil.model_directory)
+            exit(-1)
+        model_dates = []
+        for m in models:
+            model_dates.append(datetime.strptime(m, '%d-%m-%Y_%H-%M-%S'))
+        model_dates.sort(reverse=True)
+        model_path = fileUtil.model_directory + datetime.strftime(model_dates[0], '%d-%m-%Y_%H-%M-%S')
+        print(model_path)
+    else:
+        model_path = args.model
+
+    if not model_path.endswith('/'):
+        model_path += '/'
+
+    gpu_options = tf.GPUOptions(allow_growth=True, per_process_gpu_memory_fraction=0.5)
+    config = tf.ConfigProto(gpu_options=gpu_options)
+    with tf.Session(config=config) as sess:
+        saver = tf.train.import_meta_graph(model_path + "model.meta")
+        saver.restore(sess, tf.train.latest_checkpoint(model_path))
+        graph = tf.get_default_graph()
+        fileUtil.save_model(saver, sess)
